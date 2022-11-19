23f60b707b225d8895a7a64dc6c87b378c2e99f1
Phaninder Kumar <phaninder.kumar@promobitech.com>
2022-11-19 08:05:22 +0530

    Day 20: Stacks, Colors, Gradients, Buttons and alerts

---
+++ Project2-GuessTheFlag/Project2-GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project2-GuessTheFlag
+//
+//  Created by Phaninder on 18/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
+    var body: some View {
+        Text("Hello world")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project2-GuessTheFlag/Project2-GuessTheFlag/Project2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project2_GuessTheFlagApp.swift
+//  Project2-GuessTheFlag
+//
+//  Created by Phaninder on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
